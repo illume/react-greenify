95192b6dab83f3f678d65fdf57f096cd5a5c48a3
René Dudfield <renesd@gmail.com>
2019-11-10 23:09:16 +0100

    Webpack config for the component.

---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+var path = require('path');
+var config = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'index.js',
+    libraryTarget: 'commonjs2'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        include: path.resolve(__dirname, 'src'),
+        exclude: /(node_modules|bower_components|build)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      }
+    ]
+  },
+  externals: {
+    'react': 'commonjs react'
+  }
+};
+
+
+module.exports = (env, argv) => {
+
+  if (argv.mode === 'development') {
+    config.devtool = 'source-map';
+  }
+
+  if (argv.mode === 'production') {
+    //...
+  }
+
+  return config;
+};
